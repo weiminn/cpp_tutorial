fd97f812c7ae456b7d6cccb34fd7f81ffc9abaa5
weiminn <mg.weiminn@gmail.com>
2023-08-01 00:39:37 +0800

    integrated smaller subprojects and troubleshooted windows setup

---
+++ project/build/main/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/libmain.dll.a"
+  "../bin/main.exe"
+  "../bin/main.exe.manifest"
+  "../bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project/main/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    // compile the header and implementation files first before running this!!!
+    // g++ -g 07_main.cpp 07_food.cpp 07_food.h -o 07_main
+    // ./07_main
+
+    double x;
+
+    cout << "Today, I'll eat Pizza!" << endl;
+
+    cin >> x;
+
+    return 0;
+}
