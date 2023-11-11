1cad619315893bdd74f22fbbc066101bf466ffbf
Wei Minn <mg.weiminn@gmail.com>
2023-11-11 16:18:29 +0800

    control statements done

---
+++ primer/01_Getting_Started/for.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main() { 
+    int sum = 0, val = 1; 
+
+    // sum values from 1 through 10 inclusive
+    for (int val = 1; val <= 10; ++val) {
+        sum += val; // assigns sum+val to sum 
+    } 
+    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl; 
+    return 0; 
+}
